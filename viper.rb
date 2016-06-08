3ffe7e6ae1a19ba3cf855009a87f498883aa5eed
Kevin Jing Qiu <kevin@idempotent.ca>
2016-06-07 22:54:59 -0400

    Use abort

--- viper.rb
+++ viper.rb
@@ -45,11 +45,11 @@ module Leviathan
       click_on 'sign up'
 
       if page.has_text? "you are doing that too much"
-        raise RuntimeError.new('****Exception: Rate Limited****')
+        raise abort('****Exception: Rate Limited****')
       end
       if page.has_css? '.c-form-control-feedback-error'
         elem = page.find '.c-form-control-feedback-error'
-        raise RuntimeError.new("****Exception: #{elem['data-original-title']}****")
+        raise abort("****Exception: #{elem['data-original-title']}****")
       end
       doc = @db.save_doc(
         '_id'       => username,
